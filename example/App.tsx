1fdae018a97279d318b644bd6c77b46697917e76
Henrique Luís Oliveira Marques <henrique.marques@caf.io>
2023-12-29 14:47:20 -0300

    fix: example

--- example/App.tsx
+++ example/App.tsx
@@ -1,29 +1,29 @@
 import React from "react";
 import { View, Button, StyleSheet } from "react-native";
 
-import {
-  useFaceLiveness,
-  FilterType,
-  StageType,
-  TimeType,
-} from "react-native-caf-face-authenticator";
+// import {
+//   useFaceLiveness,
+//   FilterType,
+//   StageType,
+//   TimeType,
+// } from "react-native-caf-face-authenticator";
 
 const App = () => {
-  const mobileToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2NTRjZmFlMWM5YTM0NTAwMDg4YzIwODUifQ.maH9fynasnaRR2Hm5PxQ1XzLxlVZiZSvpVDD9zVtfgs";
-  const peopleId = "47496803898";
+  // const mobileToken =
+  //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2NTRjZmFlMWM5YTM0NTAwMDg4YzIwODUifQ.maH9fynasnaRR2Hm5PxQ1XzLxlVZiZSvpVDD9zVtfgs";
+  // const peopleId = "47496803898";
 
-  const { startFaceLiveness } = useFaceLiveness(mobileToken, peopleId, {
-    cafStage: StageType.PROD,
-    filter: FilterType.NATURAL,
-    imageUrlExpirationTime: TimeType.THREE_HOURS,
-    enableScreenshots: false,
-    loadingScreen: false,
-  });
+  // const { startFaceLiveness } = useFaceLiveness(mobileToken, peopleId, {
+  //   cafStage: StageType.PROD,
+  //   filter: FilterType.NATURAL,
+  //   imageUrlExpirationTime: TimeType.THREE_HOURS,
+  //   enableScreenshots: false,
+  //   loadingScreen: false,
+  // });
 
   return (
     <View style={styles.container}>
-      <Button title="teste" onPress={startFaceLiveness} />
+      <Button title="teste" onPress={() => {}} />
     </View>
   );
 };
